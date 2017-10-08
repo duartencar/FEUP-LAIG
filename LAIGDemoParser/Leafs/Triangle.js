9bdb5048f73a75669ede8d2fab3a67b0e289f592
Duarte Carvalho <up201503661@fe.up.pt>
2017-10-08 16:43:19 +0100

    corrected triangle and added methods to graph leaf

--- LAIGDemoParser/Leafs/Triangle.js
+++ LAIGDemoParser/Leafs/Triangle.js
@@ -1,15 +1,23 @@
-function myTriangle(scene, p1, p2, p3) //p# array com as coordenadas do ponto #
+function myTriangle(scene, x1, y1, z1, x2, y2, z2, x3, y3, z3, minS, maxS, minT, maxT) //p# array com as coordenadas do ponto #
 {
   CGFobject.call(this,scene);
 
-  this.p1 = p1;
+  this.p1 = [x1, y1, z1];
 
-  this.p2 = p2;
+  this.p2 = [x2, y2, z2];
 
-  this.p3 = p3;
+  this.p3 = [x3, y3, z3];
+
+  this.minS = minS || 0;
+
+  this.maxS = maxS || 1;
+
+  this.minT = minT || 0;
+
+  this.maxT = maxT || 1;
 
   this.initBuffers();
-};
+}
 
 myTriangle.prototype = Object.create(CGFobject.prototype);
 
@@ -17,13 +25,47 @@ myTriangle.prototype.constructor = myTriangle;
 
 myTriangle.prototype.initBuffers = function()
 {
-  this.vertices = p1 + p2 + p3;
+  this.vertices = this.p1;
 
-  this.indices = [0, 1, 2];
+  this.vertices = this.vertices.concat(this.p2);
 
-  //falta o vetor das normais e das texCoords
+  this.vertices = this.vertices.concat(this.p3);
 
-  this.primitiveType=this.scene.gl.TRIANGLES;
+  this.indices = [
+    0, 1, 2
+  ];
+
+  this.normals = [
+    0, 0, 1,
+    0, 0, 1,
+    0, 0, 1
+  ];
+
+  var x = Math.sqrt((this.p3[0] - this.p2[0]) * (this.p3[0] - this.p2[0]) +
+    (this.p3[1] - this.p2[1]) * (this.p3[1] - this.p2[1]) +
+    (this.p3[2] - this.p2[2]) * (this.p3[2] - this.p2[2]));
+
+  var y = Math.sqrt((this.p1[0] - this.p3[0]) * (this.p1[0] - this.p3[0]) +
+    (this.p1[1] - this.p3[1]) * (this.p1[1] - this.p3[1]) +
+    (this.p1[2] - this.p3[2]) * (this.p1[2] - this.p3[2]));
+
+  var z = Math.sqrt((this.p2[0] - this.p1[0]) * (this.p2[0] - this.p1[0]) +
+    (this.p2[1] - this.p1[1]) * (this.p2[1] - this.p1[1]) +
+    (this.p2[2] - this.p1[2]) * (this.p2[2] - this.p1[2]));
+
+  var cos = (Math.pow(x, 2) - Math.pow(y, 2) + Math.pow(z, 2)) / (2 * x * z);
+
+  var acos = Math.acos(cos);
+
+  this.initaltext = [
+    0, 0,
+    z, 0,
+    z - x * cos, x * Math.sin(acos)
+  ];
+
+  this.texCoords = this.initaltext.slice();
+
+  this.primitiveType = this.scene.gl.TRIANGLES;
 
   this.initGLBuffers();
 }
--- LAIGDemoParser/MyGraphLeaf.js
+++ LAIGDemoParser/MyGraphLeaf.js
@@ -8,14 +8,87 @@ function MyGraphLeaf(graph, xmlelem) {
     this.graph = graph;
 
     this.xmlelem = xmlelem;
+
+    this.LeafType = xmlelem.getAttribute("type");
+
+    this.LeafArgs = xmlelem.getAttribute("args").split(" ");
 }
 
-MyGraphLeaf.prototype.processXmlelem = function ()
+MyGraphLeaf.prototype.printLeafType = function ()
 {
-
+  console.log(this.LeafType);
 }
 
-MyGraphLeaf.prototype.logGeometry = function ()
+MyGraphLeaf.prototype.printLeafArgs = function ()
 {
-    return this.xmlelem.text;
+  console.log(this.LeafArgs);
 }
+
+MyGraphLeaf.prototype.printxmlelem = function ()
+{
+  console.log(this.xmlelem);
+}
+
+MyGraphLeaf.prototype.getLeaf = function (scene)
+{
+  switch(this.LeafType)
+  {
+    case "cylinder":
+        if(this.LeafArgs.length != 5)
+        {
+            console.log("Worng number of args for cylinder ( must be 5)");
+            break;
+        }
+        else
+        {
+          var Leaf = new myTube(scene, this.LeafArgs[0], this.LeafArgs[1], this.LeafArgs[2], this.LeafArgs[3], this.LeafArgs[4]);
+          break;
+        }
+
+    case "rectangle":
+      if(this.LeafArgs.length != 4)
+      {
+        console.log("Worng number of args for rectangle ( must be 4)");
+        break;
+      }
+      else
+      {
+        var Leaf = new myRectangle(scene, this.LeafArgs[0], this.LeafArgs[1], this.LeafArgs[2], this.LeafArgs[3]);
+        break;
+      }
+    case "triangle":
+      if(this.LeafArgs.length != 9)
+      {
+        console.log("Worng number of args for triangle ( must be 6)");
+        break;
+      }
+      else
+      {
+        var Leaf = new myTriangle(scene, this.LeafArgs[0], this.LeafArgs[1], this.LeafArgs[2], this.LeafArgs[3], this.LeafArgs[4], this.LeafArgs[5], this.LeafArgs[6], this.LeafArgs[7], this.LeafArgs[8]);
+        break;
+      }
+    case "sphere":
+      if(this.LeafArgs.length != 3)
+      {
+        console.log("Worng number of args for sphere ( must be 3)");
+        break;
+      }
+      else
+      {
+        var Leaf = new mySphere(scene, this.LeafArgs[0], this.LeafArgs[1], this.LeafArgs[2]);
+        break;
+      }
+  }
+
+  return Leaf;
+
+  /*scene.pushMatrix();
+
+    for(var i = 0; i < TransformStack.length; i++)
+      scene.multMatrix(TransformStack.pop());
+
+    Leaf.display();
+
+  scene.popMatrix();*/
+}
+
