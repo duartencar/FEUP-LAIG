419eb60e7ad7df0d714d1e6024b133696f235b72
Duarte Carvalho <up201503661@fe.up.pt>
2017-10-20 11:39:58 +0100

    added comentaries to my graphLeaf

--- LAIGDemoParser/MyGraphLeaf.js
+++ LAIGDemoParser/MyGraphLeaf.js
@@ -9,33 +9,51 @@ function MyGraphLeaf(graph, xmlelem)
 
     this.xmlelem = xmlelem;
 
+    //gets the the type attribute from de xml element
     this.LeafType = xmlelem.getAttribute("type");
 
+  //gets the the args attribute from de xml element
     this.LeafArgs = xmlelem.getAttribute("args").split(" ");
 
+    //Only have elements with patch Leafs
     this.patchLines = [];
 }
 
+/**
+ * prints the Leaf type
+ **/
 MyGraphLeaf.prototype.printLeafType = function ()
 {
   console.log(this.LeafType);
 }
 
+/**
+ * prints the Leaf args
+ **/
 MyGraphLeaf.prototype.printLeafArgs = function ()
 {
   console.log(this.LeafArgs);
 }
 
+/**
+ * prints the Leaf xml element
+ **/
 MyGraphLeaf.prototype.printxmlelem = function ()
 {
   console.log(this.xmlelem);
 }
 
+/**
+ * adds a group of Cpoints that form a CpLine
+ **/
 MyGraphLeaf.prototype.addPatchLine = function (x)
 {
   this.patchLines.push(x);
 }
 
+/**
+ * creates an CGF object according to the Leaf type and returns it
+ **/
 MyGraphLeaf.prototype.getLeaf = function (scene)
 {
   switch(this.LeafType)
@@ -101,8 +119,15 @@ MyGraphLeaf.prototype.getLeaf = function (scene)
   }
 
   return Leaf;
-}
+};
 
+/**
+ * Draws a Leaf
+ * toDraw - object to draw
+ * Matrix - the transformation matrix that will be 'applied'
+ * Texture - the texture that will be apllied to the object
+ * Material- the material that will be applied to the object
+ **/
 MyGraphLeaf.prototype.draw = function(scene, toDraw, Matrix, Texture, Material)
 {
   scene.pushMatrix();
@@ -110,24 +135,30 @@ MyGraphLeaf.prototype.draw = function(scene, toDraw, Matrix, Texture, Material)
     if(toDraw instanceof myPatch)
       toDraw = toDraw.makeSurface(scene);
 
+    //If there is no meterial the default one will be applied
     if(Material == "null")
       var appearance = scene.graph.materials["defaultMaterial"];
     else
       var appearance = scene.graph.materials[Material];
 
+    //It will only a aplly a texture if there is a material
     if(Texture != "clear" && Material != "null" && Texture != "null")
     {
+      //applies the texture
       appearance.setTexture(scene.graph.textures[Texture][0]);
 
+      //if the object is a rectangle or a triangle it will check for the amplif_factor attribute
       if(toDraw instanceof myRectangle || toDraw instanceof myTriangle)
         toDraw.ampText(scene.graph.textures[Texture][1], scene.graph.textures[Texture][2]);
     }
 
-
+    //applies the appearance
     appearance.apply();
 
+    //applies the transformation matrix
     scene.multMatrix(Matrix);
 
+    //displays object
     toDraw.display();
 
   scene.popMatrix();
