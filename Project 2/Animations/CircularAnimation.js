05cfdf0892ae1620a32030aaf736a0c1f54c3d50
Duarte Carvalho <up201503661@fe.up.pt>
2017-11-16 10:44:01 +0000

    added necessary methos to circular animation

--- Project 2/Animations/CircularAnimation.js
+++ Project 2/Animations/CircularAnimation.js
@@ -21,6 +21,46 @@ class CircularAnimation extends Animation
 
     //animation rotation angle
     this.rotAng = rotAng;
+
+    //contains the angle of rotation of an animation at a given moment
+    this.currAngle = initAng;
+
+    //animation rotation angle
+    this.elapsedTime = 0;
+
+    this.initialM(centerCoords, radius, initAng);
+  }
+
+  //sets the initial matrix for the animation...
+  initialM(center, radius, initAng)
+  {
+    let cos = Math.cos(initAng);
+
+    let sin = Math.sin(initAng);
+
+    let rcos = radius * cos;
+
+    let rsin = radius * sin;
+
+    let translation = vec4.create();
+
+    let transMatrix = mat4.create();
+
+    let rotMatrix = mat4.create();
+
+    vec4.set(translation, center[0] + rcos, center[1], center[2] + rsin, 1);
+
+    mat4.translate(transMatrix, transMatrix, translation);
+
+    mat4.rotateY(rotMatrix, rotMatrix, initAng * DEGREE_TO_RAD);
+
+    mat4.multiply(this.animationMatrix, rotMatrix, transMatrix);
+  }
+
+  //sets elapsed time
+  set updateElpasedTime(Time)
+  {
+    this.elapsedTime = Time;
   }
 
   //returns animation ID
@@ -57,5 +97,89 @@ class CircularAnimation extends Animation
   get totalRotation ()
   {
     return initialAngle() + rotationAngle();
+  }
+
+  //returns the distance of an animation
+  get totalDistance ()
+  {
+    return this.rotAng * Math.PI * this.radius / 180;
+  }
+
+  //returns the span of an animation
+  get animationSpan ()
+  {
+    return this.totalDistance / this.speed;
+  }
+
+  //returns angular speed
+  get angularSpeed()
+  {
+    return this.rotAng / this.animationSpan;
+  }
+
+  //returns current angle value
+  get currentAngle()
+  {
+    return this.currAngle;
+  }
+
+  //updates current angle value acording to rotation speed and time interval
+  updateCurrentAngle(diff)
+  {
+    this.currAnlge += this.angularSpeed * diff;
+  }
+
+  //returns the current direction of the animation
+  currentDirection()
+  {
+    let x = -1.0 * this.radius * Math.sin(this.currentAngle);
+
+    let z = this.radius * Math.cos(this.currentAngle);
+
+    return [x, z];
+  }
+
+  //returns the movement matrix
+  movement(diff)
+  {
+    let cos = Math.cos(this.currAngle);
+
+    let sin = Math.sin(this.currAngle);
+
+    let rcos = this.radius * cos;
+
+    let rsin = this.radius * sin;
+
+    let previous = vec3.create();
+
+    let now = vec3.create();
+
+    vec3.set(previous, rcos, 0, rsin);
+
+    this.updateCurrentAngle(diff);
+
+    let cos = Math.cos(this.currAngle);
+
+    let sin = Math.sin(this.currAngle); //CRIAR METODOS
+
+    let rcos = this.radius * cos;
+
+    let rsin = this.radius * sin;
+
+    vec3.set(now,  rcos, 0, rsin);
+
+    now.sub(previous);
+
+    var movMatrix = mat4.create();
+
+    mat4.translate(movMatrix, movMatrix, now);
+
+    return movMatrix;
+  }
+
+  //returns the final matrix
+  position(diff)
+  {
+
   }
 }
