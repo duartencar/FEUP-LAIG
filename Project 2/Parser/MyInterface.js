c6a92082604d6d37eefb1fdc3ba5c04027b39451
Duarte Carvalho <up201503661@fe.up.pt>
2017-11-24 16:51:43 +0000

    now updates current shader and current selected node

--- Project 2/Parser/MyInterface.js
+++ Project 2/Parser/MyInterface.js
@@ -2,50 +2,56 @@
  * MyInterface class, creating a GUI interface.
  * @constructor
  */
-function MyInterface() {
-    //call CGFinterface constructor
-    CGFinterface.call(this);
-}
-;
+function MyInterface()
+{
+  //call CGFinterface constructor
+  CGFinterface.call(this);
+};
 
 MyInterface.prototype = Object.create(CGFinterface.prototype);
+
 MyInterface.prototype.constructor = MyInterface;
 
 /**
  * Initializes the interface.
  * @param {CGFapplication} application
  */
-MyInterface.prototype.init = function(application) {
-    // call CGFinterface init
-    CGFinterface.prototype.init.call(this, application);
+MyInterface.prototype.init = function(application)
+{
+  // call CGFinterface init
+  CGFinterface.prototype.init.call(this, application);
 
-    // init GUI. For more information on the methods, check:
-    //  http://workshop.chromeexperiments.com/examples/gui
+  // init GUI. For more information on the methods, check:
+  //  http://workshop.chromeexperiments.com/examples/gui
 
-    this.gui = new dat.GUI();
+  this.gui = new dat.GUI();
 
-    // add a group of controls (and open/expand by defult)
+  // add a group of controls (and open/expand by defult)
 
-    return true;
+  return true;
 };
 
 /**
  * Adds a folder containing the IDs of the lights passed as parameter.
  */
-MyInterface.prototype.addLightsGroup = function(lights) {
+MyInterface.prototype.addLightsGroup = function(lights)
+{
+  var group = this.gui.addFolder("Lights");
 
-    var group = this.gui.addFolder("Lights");
-    group.open();
+  group.open();
 
-    // add two check boxes to the group. The identifiers must be members variables of the scene initialized in scene.init as boolean
-    // e.g. this.option1=true; this.option2=false;
+  // add two check boxes to the group. The identifiers must be members variables of the scene initialized in scene.init as boolean
+  // e.g. this.option1=true; this.option2=false;
 
-    for (var key in lights) {
-        if (lights.hasOwnProperty(key)) {
-            this.scene.lightValues[key] = lights[key][0];
-            group.add(this.scene.lightValues, key);
-        }
+  for (var key in lights)
+  {
+    if (lights.hasOwnProperty(key))
+    {
+      this.scene.lightValues[key] = lights[key][0];
+
+      group.add(this.scene.lightValues, key);
     }
+  }
 }
 
 /*
@@ -58,7 +64,10 @@ MyInterface.prototype.addSelectablesGroup = function(selectables)
   for(let i = 0; i < selectables.length; i++)
     selectablesDict[selectables[i].nodeID] = i;
 
-  this.gui.add(this.scene, 'selectedNode', selectablesDict).name('Node');
+  this.gui.add(this.scene, 'selectedNode', selectablesDict).name('Node').onChange(function(x)
+  {
+    this.scene.selectedNode = x;
+  });
 
   console.log("Added selectables Group");
 }
@@ -76,7 +85,10 @@ MyInterface.prototype.addShadersGroup = function()
     'Multiple textures in VS and FS': 5,
     'Sepia': 6,
     'Convolution': 7
-  }).name('Shader');
+  }).name('Shader').onChange(function(x)
+  {
+    this.scene.selectedShader = x;
+  });
 
   console.log("Added Shaders Group");
 }
