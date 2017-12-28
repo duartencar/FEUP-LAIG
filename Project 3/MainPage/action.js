f1f8805f36b165811e7b063f36e404cc2b7bc9ff
francismaria <franciscomaria97@hotmail.com>
2017-12-28 16:00:54 +0000

    Upadated main page interaction

--- Project 3/MainPage/action.js
+++ Project 3/MainPage/action.js
@@ -1,11 +1,57 @@
-function showNewGameOptions(){
+function changeSection(options){
 
-	var options = document.getElementById('newGameChoice');
-
-	if(options.style.display == "none"){
-		options.style.display = "block";
+	for(let i = 0; i < options.childNodes.length; i++){
+		if(options.childNodes[i].tagName == "DIV" && options.childNodes[i].style.display == 'block'){
+			options.childNodes[i].style.display = 'none';
+		}
 	}
-	else{
+}
+
+function showNewGameOptions(options){
+
+	changeSection(options);
+
+	let newGameChoice = document.getElementById('newGameChoice');
+	
+	newGameChoice.style.display = "block";
+}
+
+function showInstructions(options){
+
+	changeSection(options);
+
+	let instructions = document.getElementById('instructions');
+
+	instructions.style.display = "block";
+}
+
+function showSection(buttonChoice){
+
+	let options = document.getElementById('options');
+
+	if(options.style.display == "block"){
+		options.style.flex = "0";
 		options.style.display = "none";
+		return;
+	}
+
+	options.style.flex = "1";
+	options.style.display = "block";
+
+
+	switch(buttonChoice){
+		case 1:
+			showNewGameOptions(options);
+			break;
+		case 2:
+			break;
+		case 3:
+
+			break;
+		case 4:
+			showInstructions(options);
+			break;
+		default:
+			break;
 	}
 }
